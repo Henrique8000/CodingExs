bb3740b1bff441c0cef913ee7ba8ee8254670308
Henrique Flávio Guimarães <128159472+Henrique8000@users.noreply.github.com>
2025-02-23 18:03:22 -0300

    Update triangulo.py
    New geometric shapes

--- triangulo.py
+++ triangulo.py
@@ -3,4 +3,32 @@ def triangulo():
         print(i * "*")
 
 
+def quadrado():
+    print()
+    for k in range(0, 8):
+        if k == 0 or k == 7:
+            print("*" * 8)
+        else:
+            print("*      *")
+
+
+def triangulo_de_costas():
+    print()
+    c = 0
+    for i in range(6, 0, -1):
+        c += 1
+        print((" " * i) + ("*" * c))
+
+
+def triangulo_duplo():
+    print()
+    c = 0
+    for i in range(6, 0, -1):
+        c += 2
+        print((" " * i) + ("*" * c))
+
+
 triangulo()
+quadrado()
+triangulo_de_costas()
+triangulo_duplo()
